ce33bc31596dd1672e74734c20ab9ab05579e40c
Gerry Grewall <gobind99@gmail.com>
2025-05-15 19:10:15 -0400

    Create TextNode and add a main shell script to call code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+print("hello world")
+
+def main():
+    node = TextNode("Example", "boldtext", "https://www.example.com/")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum 
+
+class TextType(Enum):
+    BOLD_TEXT = "boldtext"
+    ITALIC_TEXT = "italictext"
+    CODE_TEXT = "codetext"
+    LINK_TEXT = "linktext" 
+    IMAGE_TEXT = "imagetext" 
+
+class TextNode:
+    def __init__(self, text, text_type, url = None): 
+        self.text = text
+        self.text_type = text_type 
+        self.url = url 
+
+    def __eq__(self, other): 
+        if not isinstance(other, TextNode): 
+            return NotImplemented
+        return (self.text == other.text and 
+                self.text_type == other.text_type and 
+                self.url == other.url)
+    
+    def __repr__(self): 
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
