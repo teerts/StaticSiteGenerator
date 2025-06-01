06f9347ce07553123fe5283d9df2dd6f2c173188
Gerry Grewall <gobind99@gmail.com>
2025-06-01 12:48:51 -0400

    Add helper functions to extract markdown links and images using regex

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 from textnode import TextType, TextNode
 from htmlnode import LeafNode
+import re
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
@@ -53,3 +54,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(split_part, text_type))            
     return new_nodes
 
+def extract_markdown_images(text):
+    # Regex pattern to match markdown image syntax: ![alt text](url)
+    # regexr.com for interactive regex testing. 
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"    
+    
+    matches = re.findall(pattern, text)
+    
+    return matches
+
+def extract_markdown_links(text):    
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"   
+    
+    matches = re.findall(pattern, text)
+    
+    return matches
--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,5 +1,5 @@
 import unittest 
-from helpers import text_node_to_html_node, split_nodes_delimiter
+from helpers import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestHelpers(unittest.TestCase): 
@@ -68,6 +68,18 @@ class TestHelpers(unittest.TestCase):
         node = TextNode("*The beginning* and the end*", TextType.TEXT)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "*", TextType.BOLD_TEXT)
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to website](https://www.test.com) and [to youtube](https://www.youtube.com/@test)"
+        )
+        self.assertListEqual([("to website", "https://www.test.com"), ("to youtube", "https://www.youtube.com/@test")], matches)
 
     
 
